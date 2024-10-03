04ac8f1ca5e83a8255c230ba30c045363f79349a
DorothyTom <thyntt10112002@gmail.com>
2024-10-03 22:10:52 +0700

    updateResponsiveSlider

--- src/Components/Slider/Slider.jsx
+++ src/Components/Slider/Slider.jsx
@@ -2,33 +2,48 @@ import React from 'react';
 import 'antd/dist/reset.css';
 import './Slider.css';
 import slider_1 from '../../Accsets/slider_1.jpg';
-import { Carousel } from 'antd';
+import { Carousel, Row, Col } from 'antd';
 
 const Slider = () => {
   return (
     <div className='supper-slider'>
         <div className="slider-text-overlay">
-            
             {/* <p>high summer</p> */}
-      </div>
-      <div className='slider-text-button-overlay'>
-        <p>high summer</p>
-        <button>collections</button>
-      </div>
-      <Carousel autoplay>
-        <div>
-          <img src={slider_1} alt="Slider 1" className="slider-image" />
         </div>
-        <div>
-          <img src={slider_1} alt="Slider 2" className="slider-image" />
+        <div className='slider-text-button-overlay'>
+          <p>high summer</p>
+          <button>collections</button>
         </div>
-        <div>
-          <img src={slider_1} alt="Slider 3" className="slider-image" />
-        </div>
-        <div>
-          <img src={slider_1} alt="Slider 4" className="slider-image" />
-        </div>
-      </Carousel>
+        <Carousel autoplay>
+          <div>
+            <Row >
+              <Col xs={24} sm={20} md={16} lg={12}>
+                <img src={slider_1} alt="Slider 1" className="slider-image" />
+              </Col>
+            </Row>
+          </div>
+          <div>
+            <Row >
+              <Col xs={24} sm={20} md={16} lg={12}>
+                <img src={slider_1} alt="Slider 2" className="slider-image" />
+              </Col>
+            </Row>
+          </div>
+          <div>
+            <Row >
+              <Col xs={24} sm={20} md={16} lg={12}>
+                <img src={slider_1} alt="Slider 3" className="slider-image" />
+              </Col>
+            </Row>
+          </div>
+          <div>
+            <Row >
+              <Col xs={24} sm={20} md={16} lg={12}>
+                <img src={slider_1} alt="Slider 4" className="slider-image" />
+              </Col>
+            </Row>
+          </div>
+        </Carousel>
     </div>
   );
 }
